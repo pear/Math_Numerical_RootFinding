ef1c5d93da7b262da81f472f3963ed693ef72283
Firman Wandayandi <firman@php.net>
2006-01-30 18:33:13 +0000

    Remove the __construct() method
    git-svn-id: http://svn.php.net/repository/pear/packages/Math_Numerical_RootFinding/trunk@206145 c90b9560-bf6c-de11-be94-00142212c4b1

--- Math/Numerical/RootFinding/Common.php
+++ Math/Numerical/RootFinding/Common.php
@@ -1,60 +1,59 @@
 <?php
 /* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4 foldmethod=marker: */
+
 // {{{ Header
-// +----------------------------------------------------------------------+
-// | PEAR, the PHP Extension and Application Repository                   |
-// +----------------------------------------------------------------------+
-// | PHP version 4                                                        |
-// +----------------------------------------------------------------------+
-// | Copyright (C) 2004  Firman Wandayandi                                |
-// | All rights reserved.                                                 |
-// +----------------------------------------------------------------------+
-// | Redistribution and use in source and binary forms, with or without   |
-// | modification, are permitted provided that the following conditions   |
-// | are met:                                                             |
-// |                                                                      |
-// |   Redistributions of source code must retain the above copyright     |
-// |   notice, this list of conditions and the following disclaimer.      |
-// |                                                                      |
-// |   Redistributions in binary form must reproduce the above            |
-// |   copyright notice, this list of conditions and the following        |
-// |   disclaimer in the documentation and/or other materials provided    |
-// |   with the distribution.                                             |
-// |                                                                      |
-// |   Neither the name of Firman Wandayandi nor the names of             |
-// |   contributors may be used to endorse or promote products derived    |
-// |   from this software without specific prior written permission.      |
-// |                                                                      |
-// | THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS  |
-// | "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT    |
-// | LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS    |
-// | FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE       |
-// | COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,  |
-// | INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, |
-// | BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;     |
-// | LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER     |
-// | CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT   |
-// | LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN    |
-// | ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE      |
-// | POSSIBILITY OF SUCH DAMAGE.                                          |
-// +----------------------------------------------------------------------+
-// | Authors: Firman Wandayandi <firman@php.net>                          |
-// +----------------------------------------------------------------------+
-// }}}
-// $Id$
 
 /**
- * File contains abstract class Math_Numerical_RootFinding for all
- * Math_Numerical_RootFinding model classes.
+ * File contains abstract class Math_Numerical_RootFinding_Common for all
+ * method classes.
  *
- * @author Firman Wandayandi <firman@php.net>
+ * PHP versions 4 and 5
+ *
+ * LICENSE:
+ *
+ * BSD License
+ *
+ * Copyright (c) 2004-2006 Firman Wandayandi
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above
+ *    copyright notice, this list of conditions and the following
+ *    disclaimer in the documentation and/or other materials provided
+ *    with the distribution.
+ * 3. Neither the name of Firman Wandayandi nor the names of
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @category Math
  * @package Math_Numerical_RootFinding
  * @subpackage Methods
- * @category Math
+ * @author Firman Wandayandi <firman@php.net>
+ * @copyright Copyright (c) 2004-2006 Firman Wandayandi
  * @license http://www.opensource.org/licenses/bsd-license.php
  *          BSD License
+ * @version CVS: $Id$
  */
 
+// }}}
 // {{{ Dependencies
 
 /**
@@ -63,13 +62,20 @@
 require_once 'PEAR.php';
 
 // }}}
+// {{{ Class: Math_Numerical_RootFinding_Common
 
 /**
  * Abstract class contains common properties and methods for specified
  * method classes.
  *
- * @author Firman Wandayandi <firman@php.net>
+ * @category Math
  * @package Math_Numerical_RootFinding
+ * @subpackage Methods
+ * @author Firman Wandayandi <firman@php.net>
+ * @copyright Copyright (c) 2004-2006 Firman Wandayandi
+ * @license http://www.opensource.org/licenses/bsd-license.php
+ *          BSD License
+ * @version Release: @package_version@
  * @abstract
  */
 class Math_Numerical_RootFinding_Common
@@ -123,27 +129,14 @@ class Math_Numerical_RootFinding_Common
     // {{{ Constructor
 
     /**
-     * PHP4 Compatible Constructor.
-     *
-     * @param array $options (optional) Options.
-     *
-     * @access public
-     * @see __construct()
-     */
-    function Math_Numerical_RootFinding_Common($options = null)
-    {
-        $this->__construct($options);
-    }
-
-    /**
-     * PHP5 Compatible Constructor.
+     * Constructor.
      *
      * @param array $options (optional) Options.
      *
      * @access public
      * @see set()
      */
-    function __construct($options = null)
+    function Math_Numerical_RootFinding_Common($options = null)
     {
         if ($options !== null) {
             $this->set($options);
@@ -352,6 +345,8 @@ class Math_Numerical_RootFinding_Common
 
     // }}}
 }
+
+// }}}
 
 /*
  * Local variables:
