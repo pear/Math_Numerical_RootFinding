027a623de9cf1270e1baa6bc7be083414b9181cf
Firman Wandayandi <firman@php.net>
2006-01-30 18:12:42 +0000

    Fixed for current coding standard complain and added more inline doc
    git-svn-id: http://svn.php.net/repository/pear/packages/Math_Numerical_RootFinding/trunk@206142 c90b9560-bf6c-de11-be94-00142212c4b1

--- Math/Numerical/RootFinding.php
+++ Math/Numerical/RootFinding.php
@@ -1,58 +1,57 @@
 <?php
 /* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4 foldmethod=marker: */
+
 // {{{ Header
-// +----------------------------------------------------------------------+
-// | PEAR, the PHP Extension and Application Repository                   |
-// +----------------------------------------------------------------------+
-// | PHP version 4                                                        |
-// +----------------------------------------------------------------------+
-// | Copyright (C) 2004  Firman Wandayandi                                |
-// | All rights reserved.                                                 |
-// +----------------------------------------------------------------------+
-// | Redistribution and use in source and binary forms, with or without   |
-// | modification, are permitted provided that the following conditions   |
-// | are met:                                                             |
-// |                                                                      |
-// |   Redistributions of source code must retain the above copyright     |
-// |   notice, this list of conditions and the following disclaimer.      |
-// |                                                                      |
-// |   Redistributions in binary form must reproduce the above            |
-// |   copyright notice, this list of conditions and the following        |
-// |   disclaimer in the documentation and/or other materials provided    |
-// |   with the distribution.                                             |
-// |                                                                      |
-// |   Neither the name of Firman Wandayandi nor the names of             |
-// |   contributors may be used to endorse or promote products derived    |
-// |   from this software without specific prior written permission.      |
-// |                                                                      |
-// | THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS  |
-// | "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT    |
-// | LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS    |
-// | FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE       |
-// | COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,  |
-// | INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, |
-// | BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;     |
-// | LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER     |
-// | CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT   |
-// | LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN    |
-// | ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE      |
-// | POSSIBILITY OF SUCH DAMAGE.                                          |
-// +----------------------------------------------------------------------+
-// | Authors: Firman Wandayandi <firman@php.net>                          |
-// +----------------------------------------------------------------------+
-// }}}
-// $Id$
 
 /**
  * File contains Math_Numerical_RootFinding base class.
  *
- * @author Firman Wandayandi <firman@php.net>
- * @package Math_Numerical_RootFinding
+ * PHP versions 4 and 5
+ *
+ * LICENSE:
+ *
+ * BSD License
+ *
+ * Copyright (c) 2004-2006 Firman Wandayandi
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above
+ *    copyright notice, this list of conditions and the following
+ *    disclaimer in the documentation and/or other materials provided
+ *    with the distribution.
+ * 3. Neither the name of Firman Wandayandi nor the names of
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
  * @category Math
+ * @package Math_Numerical_RootFinding
+ * @author Firman Wandayandi <firman@php.net>
+ * @copyright Copyright (c) 2004-2006 Firman Wandayandi
  * @license http://www.opensource.org/licenses/bsd-license.php
  *          BSD License
+ * @version CVS: $Id$
  */
 
+// }}}
 // {{{ Dependencies
 
 /**
@@ -61,15 +60,40 @@
 require_once 'PEAR.php';
 
 // }}}
+// {{{ Global variables
+
+/**
+ * Method driver aliases in order to create the prety file names,
+ * also for insensitive-case of method name calls.
+ *
+ * @global array $GLOBALS['_Math_Numerical_RootFinding_drivers']
+ * @_Math_Numerical_RootFinding_drivers
+ */
+$GLOBALS['_Math_Numerical_RootFinding_drivers'] = array(
+    'bisection'         => 'Bisection',
+    'falseposition'     => 'FalsePosition',
+    'fixedpoint'        => 'FixedPoint',
+    'newtonraphson'     => 'NewtonRaphson',
+    'newtonraphson2'    => 'NewtonRaphson2',
+    'ralstonrabinowitz' => 'RalstonRabinowitz',
+    'secant'            => 'Secant'
+);
+
+// }}}
+// {{{ Class: Math_Numerical_RootFinding
 
 /**
  * Math_Numerical_RootFinding base class.
  *
  * This class intended for build API structure and abstract class members.
  *
- * @author Firman Wandayandi <firman@php.net>
+ * @category Math
  * @package Math_Numerical_RootFinding
- * @abstract
+ * @author Firman Wandayandi <firman@php.net>
+ * @copyright Copyright (c) 2004-2006 Firman Wandayandi
+ * @license http://www.opensource.org/licenses/bsd-license.php
+ *          BSD License
+ * @version Release: @package_version@
  */
 class Math_Numerical_RootFinding
 {
@@ -84,12 +108,17 @@ class Math_Numerical_RootFinding
      *
      * @return object New method's class on success or PEAR_Error on failure.
      * @access public
-     * @see Math_Numerical_RootFinding::set()
      * @static
      */
     function &factory($method, $options = null)
     {
-        $method = ucfirst(trim($method));
+        $method = strtolower(trim($method));
+        if (!isset($GLOBALS['_Math_Numerical_RootFinding_drivers'][$method])) {
+            return PEAR::raiseError('Driver file not found for ' .
+                                    '\'' . $method . '\' method');
+        }
+
+        $method = $GLOBALS['_Math_Numerical_RootFinding_drivers'][$method];
         $filename = dirname(__FILE__) . '/RootFinding/' . $method . '.php';
 
         if (!file_exists($filename)) {
@@ -121,6 +150,8 @@ class Math_Numerical_RootFinding
 
     // }}}
 }
+
+// }}}
 
 /*
  * Local variables:
