b2b63262ff03ad9148aed47305cfa13a7bda0402
Firman Wandayandi <firman@php.net>
2006-01-31 18:54:58 +0000

    update for release 1.0.0
    git-svn-id: http://svn.php.net/repository/pear/packages/Math_Numerical_RootFinding/trunk@206200 c90b9560-bf6c-de11-be94-00142212c4b1

--- generate_package_xml.php
+++ generate_package_xml.php
@@ -1,52 +1,50 @@
 <?php
 require_once('PEAR/PackageFileManager.php');
+require_once 'PEAR/Config.php';
 
 $pkg = new PEAR_PackageFileManager;
+$config = new PEAR_Config;
 
 $packagedir = dirname(__FILE__);
-$self = basename(__FILE__);
-$category = 'Math';
 
-$packagedesc =
-'Math_Numerical_RootFinding is the package' . "\n" .
-'provide various Numerical Methods Root-Finding' . "\n" .
-'functions implemented in PHP, e.g Bisection,' . "\n" .
-'Newton-Raphson, Fixed Point, Secant etc.' . "\n";
+$desc = <<<EOT
+Math_Numerical_RootFinding is the package
+provide various Numerical Methods Root-Finding
+functions implemented in PHP, e.g Bisection .
+Newton-Raphson, Fixed Point, Secant etc
+EOT;
 
-$packagenotes =
-'- changed license into BSD License' . "\n" .
-'- removed file \'Bracketing.php\'' . "\n" .
-'- removed file \'Open.php\'' . "\n" .
-'- introduce new abstract class' . "\n" .
-'  Math_Numerical_RootFinding_Common' . "\n" .
-'- renamed all method filenames to uppercase' . "\n" .
-'  first letter' . "\n" .
-'- fixed Bug #2897: Capitalization type in' . "\n" .
-'  falseposition.php' . "\n" .
-'- renamed all compute function names from' . "\n" .
-'  method name e.g bisection() into compute()' . "\n" .
-'- added new function infoCompute() to provide' . "\n" .
-'  information about compute function arguments' . "\n" .
-'- applied divergent testing into all methods' . "\n";
+$notes = <<<EOT
+* Fixed method name capitol bug in Math_Numerical_RootFinding::factory(), thanks to Keith Palmer Jr.
+* Now all method driver names are using insensitive-case alias
+* Converted all header comment block into current coding standard
+* Added Matthew Fonda as package co-lead
+* This is first stable version release
+EOT;
 
 $options = array(
-    'doctype'           => 'D:\Net\www\htdocs\PEAR\PEAR\data\PEAR\package.dtd',
+    'simpleoutput'      => true,
+    'doctype'           => $config->get('data_dir') . '/PEAR/package.dtd',
     'package'           => 'Math_Numerical_RootFinding',
     'license'           => 'BSD License',
     'baseinstalldir'    => '',
-    'version'           => '0.3.0',
+    'version'           => '1.0.0',
     'packagedirectory'  => $packagedir,
     'pathtopackagefile' => $packagedir,
-    'state'             => 'alpha',
-    'filelistgenerator' => 'file',
-    'notes'             => $packagenotes,
-    'summary'           => 'Numerical Methods Root-Finding functions package',
-    'description'       => $packagedesc,
+    'state'             => 'stable',
+    'filelistgenerator' => 'cvs',
+    'notes'             => $notes,
+    'summary'           => 'Numerical Methods Root-Finding collection package',
+    'description'       => $desc,
+    'dir_roles'         => array(
+        'docs'      => 'doc',
+        'data'      => 'data'
+    ),
     'ignore'            => array(
-                            'package.xml',
-                            '*.tgz',
-                            $self
-                           )
+        'package.xml',
+        '*.tgz',
+        basename(__FILE__)
+    )
 );
 
 $e = $pkg->setOptions($options);
@@ -68,11 +66,15 @@ $pkg->addRole('afm', 'php');
 $pkg->addRole('pkg', 'doc');
 $pkg->addRole('cls', 'doc');
 $pkg->addRole('proc', 'doc');
-$pkg->addRole('sh', 'script');
 $pkg->addRole('txt', 'doc');
+$pkg->addRole('sh', 'script');
+
+$pkg->addMaintainer('firman', 'lead', 'Firman Wandayandi', 'firman@php.net');
+$pkg->addMaintainer('mfonda', 'lead', 'Matthew Fonda', 'mfonda@php.net');
 
 $pkg->addDependency('php', '4.2.0', 'ge', 'php');
-$pkg->addMaintainer('firman', 'lead', 'Firman Wandayandi', 'firman@php.net');
+
+$e = $pkg->addGlobalReplacement('package-info', '@package_version@', 'version');
 
 $e = $pkg->writePackageFile();
 if (PEAR::isError($e)) {
