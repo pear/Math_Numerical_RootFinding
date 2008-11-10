c6e525aaf7a2ee81efe72b3646826ca921fb4a76
Firman Wandayandi <firman@php.net>
2008-11-10 00:42:14 +0000

    Reformatted inline doc
    git-svn-id: http://svn.php.net/repository/pear/packages/Math_Numerical_RootFinding/trunk@268682 c90b9560-bf6c-de11-be94-00142212c4b1

--- Math/Numerical/RootFinding.php
+++ Math/Numerical/RootFinding.php
@@ -2,66 +2,43 @@
 /* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4 foldmethod=marker: */
 
 // {{{ Header
-
+    
 /**
  * File contains Math_Numerical_RootFinding base class.
  *
  * PHP versions 4 and 5
  *
  * LICENSE:
+ * Copyright (c) 2008 Firman Wandayandi <firman@php.net>
  *
- * BSD License
+ * This source file is subject to the BSD License license that is bundled
+ * with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://www.opensource.org/licenses/bsd-license.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to pear-dev@list.php.net so we can send you a copy immediately.
  *
- * Copyright (c) 2004-2006 Firman Wandayandi
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above
- *    copyright notice, this list of conditions and the following
- *    disclaimer in the documentation and/or other materials provided
- *    with the distribution.
- * 3. Neither the name of Firman Wandayandi nor the names of
- *    contributors may be used to endorse or promote products derived
- *    from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
- * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
- * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
- * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
- * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
- * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
- *
- * @category Math
- * @package Math_Numerical_RootFinding
- * @author Firman Wandayandi <firman@php.net>
- * @copyright Copyright (c) 2004-2006 Firman Wandayandi
- * @license http://www.opensource.org/licenses/bsd-license.php
- *          BSD License
- * @version CVS: $Id$
+ * @category    Math
+ * @package     Math_Numerical_RootFinding
+ * @author      Firman Wandayandi <firman@php.net>
+ * @copyright   Copyright (c) 2004-2008 Firman Wandayandi
+ * @license     http://www.opensource.org/licenses/bsd-license.php
+ *              BSD License
+ * @version     CVS: $Id$
  */
 
 // }}}
 // {{{ Dependencies
-
+    
 /**
- * Require PEAR for handling errors.
+ * PEAR
  */
 require_once 'PEAR.php';
 
 // }}}
 // {{{ Global variables
-
+    
 /**
  * Method driver aliases in order to create the prety file names,
  * also for insensitive-case of method name calls.
@@ -81,19 +58,19 @@ $GLOBALS['_Math_Numerical_RootFinding_drivers'] = array(
 
 // }}}
 // {{{ Class: Math_Numerical_RootFinding
-
+    
 /**
  * Math_Numerical_RootFinding base class.
  *
  * This class intended for build API structure and abstract class members.
  *
- * @category Math
- * @package Math_Numerical_RootFinding
- * @author Firman Wandayandi <firman@php.net>
- * @copyright Copyright (c) 2004-2006 Firman Wandayandi
- * @license http://www.opensource.org/licenses/bsd-license.php
- *          BSD License
- * @version Release: @package_version@
+ * @category    Math
+ * @package     Math_Numerical_RootFinding
+ * @author      Firman Wandayandi <firman@php.net>
+ * @copyright   Copyright (c) 2004-2008 Firman Wandayandi
+ * @license     http://www.opensource.org/licenses/bsd-license.php
+ *              BSD License
+ * @version     Release: @package_version@
  */
 class Math_Numerical_RootFinding
 {
@@ -102,12 +79,11 @@ class Math_Numerical_RootFinding
     /**
      * Create new instance of RootFinding method class.
      *
-     * @param string $method Method name.
-     * @param array $options (optional) Options (options is available in
-     *                                  specified method class).
+     * @param   string  $method   Method name.
+     * @param   array   $options  (optional) Options (options is available inspecified method class).
      *
-     * @return object New method's class on success or PEAR_Error on failure.
-     * @access public
+     * @return  object            New method's class on success or PEAR_Error on failure.
+     * @access  public
      * @static
      */
     function &factory($method, $options = null)
